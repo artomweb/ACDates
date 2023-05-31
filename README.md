9ffc5b25ddb057826f85d24dd90ef9ae8c8acdf5
artom web <artomweb@gmail.com>
2023-05-31 20:24:45 +0100

    added new lines and readme image

--- README.md
+++ README.md
@@ -2,10 +2,6 @@
 
 A small web app which generates a list of dates between two days. Useful when writing your own diary or calendar in a text editor.
 
-## Exmaple of ouptut:
+## Exmaple:
 
-Thursday 6/4/2023:
-
-Friday 7/4/2023:
-
-Saturday 8/4/2023:
+![dateGen](dateGenDemo.JPG)
--- index.js
+++ index.js
@@ -2,52 +2,42 @@ let startDateInput = document.getElementById("startDate");
 let endDateInput = document.getElementById("endDate");
 let outputDiv = document.getElementById("output");
 
-var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
+startDateInput.value = new Date().toISOString().split("T")[0];
+endDateInput.value = new Date().toISOString().split("T")[0];
 
-startDateInput.value = new Date().toISOString().split('T')[0]
-endDateInput.value = new Date().toISOString().split('T')[0]
+function generate() {
+  console.log("hello");
+  let startDate = new Date(startDateInput.value);
+  startDate.setTime(startDate.getTime() + 12 * 60 * 60 * 1000);
+  let endDate = new Date(endDateInput.value);
+  endDate.setTime(endDate.getTime() + 12 * 60 * 60 * 1000);
+  endDate.setTime(endDate.getTime() + 24 * 60 * 60 * 1000);
+  const diffTime = Math.abs(startDate - endDate);
+  const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
 
+  console.log(diffDays);
 
-function generate(){
-    console.log("hello")
-    let startDate = new Date(startDateInput.value);
-    startDate.setTime(startDate.getTime() + (12*60*60*1000));
-    let endDate = new Date(endDateInput.value);
-    endDate.setTime(endDate.getTime() + (12*60*60*1000));
-    endDate.setTime(endDate.getTime() + (24*60*60*1000));
-    const diffTime = Math.abs(startDate - endDate);
-    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
+  let dateArray = [];
 
-    console.log(diffDays)
+  for (let i = 0; i < diffDays; i++) {
+    let thisDay = new Date(startDate.getTime());
+    thisDay.setDate(thisDay.getDate() + i);
 
-    let dateArray = [];
+    var month = thisDay.getUTCMonth() + 1;
+    var day = thisDay.getUTCDate();
+    var year = thisDay.getUTCFullYear();
+    let dayName = days[thisDay.getDay()];
 
-    for (let i = 0; i < diffDays; i++) {
-        let thisDay = new Date(startDate.getTime());
-        thisDay.setDate(thisDay.getDate() + i);
+    let ouputDateHtml = "<b>" + dayName + " " + day + "/" + month + "/" + year + ":<b>";
 
-        var month = thisDay.getUTCMonth() + 1;
-        var day = thisDay.getUTCDate();
-        var year = thisDay.getUTCFullYear();
-        let dayName = days[thisDay.getDay()];
+    console.log(ouputDateHtml);
 
-        let ouputDateHtml = "<b>" + dayName + " " + day + "/" + month + "/" + year + ":<b>";
+    dateArray.push(ouputDateHtml);
+  }
 
-        console.log(ouputDateHtml);
+  let outputHtml = dateArray.join("<br><br>");
 
-
-        dateArray.push(ouputDateHtml);
-
-
-
-
-    }
-
-    let outputHtml = dateArray.join("<br>");
-
-    outputDiv.innerHTML = outputHtml;
-
-
-
+  outputDiv.innerHTML = outputHtml;
 }
