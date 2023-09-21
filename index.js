006220ea522626aec44503186275b73ff689726e
Archie <artomweb@gmail.com>
2023-09-21 22:30:40 +0100

    comments

--- index.js
+++ index.js
@@ -2,42 +2,60 @@ let startDateInput = document.getElementById("startDate");
 let endDateInput = document.getElementById("endDate");
 let outputDiv = document.getElementById("output");
 
-var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
 
-startDateInput.value = new Date().toISOString().split("T")[0];
-endDateInput.value = new Date().toISOString().split("T")[0];
+// Set the initial values of the startDateInput and endDateInput fields to the current date
+const today = new Date().toISOString().split("T")[0];
+startDateInput.value = today;
+endDateInput.value = today;
 
 function generate() {
-  console.log("hello");
+  // Parse the date values entered in the input fields
+  // 12 hours is added to avoid time difference issues such as daylight savings
   let startDate = new Date(startDateInput.value);
-  startDate.setTime(startDate.getTime() + 12 * 60 * 60 * 1000);
-  let endDate = new Date(endDateInput.value);
-  endDate.setTime(endDate.getTime() + 12 * 60 * 60 * 1000);
-  endDate.setTime(endDate.getTime() + 24 * 60 * 60 * 1000);
-  const diffTime = Math.abs(startDate - endDate);
-  const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+  startDate.setTime(startDate.getTime() + 12 * 60 * 60 * 1000); // Add 12 hours to the start date
 
-  console.log(diffDays);
+  // 24 hours is added so that the final date is included
+  let endDate = new Date(endDateInput.value);
+  endDate.setTime(endDate.getTime() + 12 * 60 * 60 * 1000); // Add 12 hours to the end date
+  endDate.setTime(endDate.getTime() + 24 * 60 * 60 * 1000); // Add 24 hours to the end date
+
+  // Calculate the time difference between the start and end dates in milliseconds
+  const diffTime = Math.abs(startDate - endDate);
+
+  // Calculate the number of days in the date range
+  const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
 
   let dateArray = [];
 
+  // Loop through each day in the date range
   for (let i = 0; i < diffDays; i++) {
     let thisDay = new Date(startDate.getTime());
-    thisDay.setDate(thisDay.getDate() + i);
+    thisDay.setDate(thisDay.getDate() + i); // Calculate the date for the current iteration
 
-    var month = thisDay.getUTCMonth() + 1;
+    // Get the month, day, and year components of the current date
+    var month = thisDay.getUTCMonth() + 1; // Months are zero-based, so add 1
     var day = thisDay.getUTCDate();
     var year = thisDay.getUTCFullYear();
+
+    // Get the name of the day of the week for the current date
     let dayName = days[thisDay.getDay()];
 
-    let ouputDateHtml = "<b>" + dayName + " " + day + "/" + month + "/" + year + ":<b>";
+    let outputDateHtml =
+      "<b>" + dayName + " " + day + "/" + month + "/" + year + ":<b>";
 
-    console.log(ouputDateHtml);
-
-    dateArray.push(ouputDateHtml);
+    dateArray.push(outputDateHtml); // Add the formatted date to the dateArray
   }
 
+  // Join the formatted date strings with line breaks and update the content of the outputDiv
   let outputHtml = dateArray.join("<br><br>");
-
   outputDiv.innerHTML = outputHtml;
 }
